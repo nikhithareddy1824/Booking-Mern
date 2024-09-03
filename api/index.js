558407817b62bdcdec6b6ea44131458f8caeb5cc
nikhithareddy1824 <117994778+nikhithareddy1824@users.noreply.github.com>
2024-09-03 20:32:36 +0530

    Add files via upload

--- api/index.js
+++ api/index.js
@@ -36,10 +36,10 @@ app.use("/api/users", usersRoute);
 app.use("/api/hotels", hotelsRoute);
 app.use("/api/rooms", roomsRoute);
 
-app.use(express.static(path.join(__dirname, '/client/dist')));
+app.use(express.static(path.join(__dirname, '/client/build')));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+  res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
 })
 
 app.use((err, req, res, next) => {
